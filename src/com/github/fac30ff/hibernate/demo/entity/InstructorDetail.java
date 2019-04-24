19a4a0fbad7a89723cff5ec3041bb0ee975105d8
Igor Rudenko <ihor.rudenko@globallogic.com>
2019-04-24 15:00:14 +0300

    finished one to one uni

--- src/com/github/fac30ff/hibernate/demo/entity/InstructorDetail.java
+++ src/com/github/fac30ff/hibernate/demo/entity/InstructorDetail.java
@@ -1,10 +1,12 @@
 package com.github.fac30ff.hibernate.demo.entity;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -50,10 +52,11 @@ public class InstructorDetail {
 	public void setHobby(String hobby) {
 		this.hobby = hobby;
 	}
-
+	
 	@Override
 	public String toString() {
-		return "UnstructorDetail [id=" + id + ", youtubeChannel=" + youtubeChannel + ", hobby=" + hobby + "]";
+		return "InstructorDetail [id=" + id + ", youtubeChannel=" + youtubeChannel + ", hobby=" + hobby
+				 + "]";
 	}
 	
 }
